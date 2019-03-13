1b3802f76d48af239cd8276b69f731d01cc31a87
justynias <justynakozik1@gmail.com>
2019-03-13 13:39:49 +0100

    initial not working sample ui

---
+++ currency-app/currency-app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  currency-app
+//
+//  Created by RMS2018 on 13.03.2019.
+//  Copyright © 2019 RMS2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
