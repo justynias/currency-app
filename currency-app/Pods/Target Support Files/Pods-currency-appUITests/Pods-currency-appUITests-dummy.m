1e56c53e044d39e67680e3b85c5bf7b038024008
justynias <justynakozik1@gmail.com>
2019-03-14 10:00:54 -0700

    initial working drop down list

---
+++ currency-app/Pods/Target Support Files/Pods-currency-appUITests/Pods-currency-appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_currency_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_currency_appUITests
+@end
--- currency-app/currency-app/ViewController.swift
+++ currency-app/currency-app/ViewController.swift
@@ -7,13 +7,47 @@
 //
 
 import UIKit
+import iOSDropDown
 
+@IBDesignable extension UIButton{
+    @IBInspectable var borderColor: UIColor?{
+        set{
+            guard let uiColor = newValue else {return}
+            layer.borderColor=uiColor.cgColor
+        }
+        get{
+            guard let color = layer.borderColor else {return nil}
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat{
+        set{
+            layer.cornerRadius = newValue
+        }
+        get{
+            return layer.cornerRadius
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat{
+        set{
+            layer.borderWidth = newValue
+        }
+        get{
+            return layer.borderWidth
+        }
+    }
+}
 class ViewController: UIViewController {
 
+   @IBOutlet weak var currencyDropDown: DropDown!
+
+    
     override func viewDidLoad() {
+
+    
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+                 currencyDropDown.optionArray = ["AAA", "ABA", "BBB", "CAB"]
+       
     }
 
 
