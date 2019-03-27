4190c04a3dce5b2db8219c5df7b0c2ff77217dfc
justynias <justynakozik1@gmail.com>
2019-03-27 13:14:20 +0100

    initial fetching data from api

--- currency-app/currency-app/ViewController.swift
+++ currency-app/currency-app/ViewController.swift
@@ -43,15 +43,61 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var currencyOutDropDown: DropDown!
     
+    
     override func viewDidLoad() {
 
     
         super.viewDidLoad()
         currencyInDropDown.optionArray = ["AAA", "ABA", "BBB", "CAB"]
         currencyOutDropDown.optionArray = ["AAA", "ABA", "BBB", "CAB"]
-
+        
+        var keys:NSDictionary?
+        
+        if let path =  Bundle.main.path(forResource: "keys", ofType: "plist"){
+            keys=NSDictionary(contentsOfFile: path)
+        }
+        let arrayOfKeys = keys?.allValues as! [String]
+        fetchDataFromApi(apiKey: arrayOfKeys[0])
        
     }
+    
+    func fetchDataFromApi(apiKey: String){
+        let api = "http://www.apilayer.net/api/historical?access_key="
+        let endpoint = "&date=2019-03-03&currencies=USD,PLN&format=1"
+        let url = URL(string: api + apiKey + endpoint)!
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let content = data else {
+                print("No data")
+                return
+            }
+            
+            // serialise the data / NSData object into Dictionary [String : Any]
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            
+            print("gotten json response dictionary is \n \(json)")
+            // update UI using the response here
+        }
+        
+        // execute the HTTP request
+        task.resume()
+
+    }
+
 
 
 }
